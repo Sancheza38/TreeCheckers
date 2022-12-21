4e1d55fa369d9ba9a6c86042dbd96bf6a61aac0a
Sancheza38 <93687081+Sancheza38@users.noreply.github.com>
2022-12-21 00:29:19 -0600

    replaced function for initializing units
    replaced original function for initializing units onto the board with one that accurately picks the center of a tile.

--- wip/main.py
+++ wip/main.py
@@ -35,70 +35,28 @@ def create_hexagon(position, radius=34.64, flat_top=False) -> HexagonTile:
     class_ = FlatTopHexagonTile if flat_top else HexagonTile
     return class_(radius, position, colour=(255, 255, 255))
 
-def init_units_test(UNITS_NUM=18) -> List[Unit]:
+def init_units_test(UNITS_NUM, hexagons) -> List[Unit]:
     """Creates a list of unit pieces for the gameboard"""
     HALF_UNITS_NUM=int(UNITS_NUM/2)
-    h=960
-    w=1280
-    rad=30 #34.64
-    cols=18#int((w-8-rad)/(2*rad))&~1-1
-    border_x =(w+rad-cols*2*rad)/2
-    rows=14#int(h/(2*rad))&~1
-    border_y = 108#(h-rows*2*rad)/2
+    right_side = [hexagon.centre for hexagon in hexagons]
+    left_side = [item for item in reversed(right_side)]
 
     circle = [[0 for x in range(10)] for x in range(UNITS_NUM)]
     for unit in range(HALF_UNITS_NUM):
         ti_restart = True
-        circle[unit][0]=unit
-        circle[unit+HALF_UNITS_NUM][0]=unit+HALF_UNITS_NUM
+
+        circle[unit][0],circle[unit+HALF_UNITS_NUM][0]=unit,unit+HALF_UNITS_NUM
         while ti_restart:
+            
             ti_restart=False
-            num = random.randint(0,4294967295)%h
-            circle[unit][3]= num-(num%(rad*2))+rad
-            circle[unit+HALF_UNITS_NUM][3]=h-1-circle[unit][3]
-            j1 = int(circle[unit][3]/(rad*2))
-            j2 = int(circle[unit+HALF_UNITS_NUM][3]/(rad*2))
-            circle[unit][3]+=border_y
-            
-            if not unit:
-                circle[unit][2]=rad
-                if j1&1:
-                    ti_restart=True
-            else:
-                num = random.randint(0,4294967295)%(w-rad*2*2)/2
-                circle[unit][2]= num-num%(rad*2)#math.floor((random.randrange(0,4294967295)%((w-rad*2*2)/2))-(rad*2))+rad
-        circle[unit+HALF_UNITS_NUM][2]=w-1-int(circle[unit][2])
+            index = random.randint(0,251)
+            circle[unit][2],circle[unit][3]= right_side[index]
+            circle[unit+HALF_UNITS_NUM][2],circle[unit+HALF_UNITS_NUM][3]= left_side[index]
 
-        if j1&1:
-            circle[unit][2]+=rad
-        if j2&1:
-            circle[unit+HALF_UNITS_NUM][2]+=rad
-        circle[unit][2]+=border_x
-        circle[unit+HALF_UNITS_NUM][2]-=border_x
-
-        def S2Circle(x,y):
-            
-            tempY=int((y-border_y)/(rad*2))
-            if tempY&1:
-                tempX=(x-rad-border_x)/(rad*2)
-            else:
-                tempX=(x-border_x)/(rad*2)
-            return tempX,tempY
-
-        def Circle2S(x,y):
-            
-            print(x,y)
-            tempY=y*rad*2+rad+border_y
-            tempX=x*rad*2+rad+border_x
-            if int(tempY)&1:
-                tempX+=rad
-            return tempX,tempY
-
-        circle[unit][2],circle[unit][3]=S2Circle(circle[unit][2],circle[unit][3])
-        circle[unit][2],circle[unit][3]=Circle2S(circle[unit][2],circle[unit][3])
-
-        circle[unit+HALF_UNITS_NUM][2],circle[unit+HALF_UNITS_NUM][3]=S2Circle(circle[unit+HALF_UNITS_NUM][2],circle[unit+HALF_UNITS_NUM][3])
-        circle[unit+HALF_UNITS_NUM][2],circle[unit+HALF_UNITS_NUM][3]=Circle2S(circle[unit+HALF_UNITS_NUM][2],circle[unit+HALF_UNITS_NUM][3])
+            if not unit and right_side[index][0] > 115:
+                ti_restart=True
+            elif right_side[index][0] > 535:
+                ti_restart=True
 
         circle[unit][4]=0
         circle[unit+HALF_UNITS_NUM][4]=1
@@ -283,9 +241,10 @@ def main():
     screen = pygame.display.set_mode((1280, 960))
     clock = pygame.time.Clock()
     hexagons = init_hexagons(flat_top=False)
-    print(hexagons[251].centre)
+    print(hexagons[0].centre)
+    print(hexagons[7].centre)
     #unit_list = init_units(UNITS_NUM=UNITS_NUM)
-    circleUnits = init_units_test(18)
+    circleUnits = init_units_test(18, hexagons)
 
     # circleUnits = init_units(18, hexagons)
 
--- wip/scrapCode.py
+++ wip/scrapCode.py
@@ -1,37 +1,117 @@
-def init_units(UNITS_NUM=18) -> List[Unit]:
-    """Creates a list of unit pieces for the gameboard"""
-    i,j,j1,j2,dd,best_dd,best = None
-    h=960
-    w=1280
-    rad=34.64
-    border_y = 108
-    border_x = 115
-    circle = Unit[UNITS_NUM]
-    for unit in range(UNITS_NUM/2):
-        circle[unit].num=unit
-        circle[unit+UNITS_NUM/2].num=unit+UNITS_NUM/2
-        def ti_restart():
-            circle[unit].y= math.floor((random.randrange(0,4294967295)%960)-(34.64*2))+34.64
-            circle[unit+UNITS_NUM/2].y=h-1-circle[unit].y
-            j1 = circle[unit].y/(rad*2)
-            j2 = circle[unit+UNITS_NUM/2].y/(rad*2)
-            circle[unit].y+=border_y
-
-            if not unit:
-                circle[unit].x=rad
-                if j1&1:
-                    ti_restart()
-            else:
-                circle[unit].x=math.floor((random.randrange(0,4294967295)%((w-rad*2*2)/2))-(34.64*2))+34.64
-        
-        ti_restart()
-        circle[unit+UNITS_NUM/2].x=w-1-circle[unit].x
-        if j1&1:
-            circle[unit].x+=rad
-        if j2&1:
-            circle[unit+UNITS_NUM/2].x+=rad
-        circle[unit].x+=border_x
-        circle[unit+UNITS_NUM/2].x+=border_x
-
 
 # circle[unit][3]= math.floor((random.randrange(0,4294967295)%960)-(rad*2))+rad
+
+
+# original method of initializing units found in TempleOS
+def init_units_test(UNITS_NUM=18) -> List[Unit]:
+    """Creates a list of unit pieces for the gameboard"""
+    HALF_UNITS_NUM=int(UNITS_NUM/2)
+    h=960
+    w=1280
+    rad=30 #34.64
+    cols=18#int((w-8-rad)/(2*rad))&~1-1
+    border_x =(w+rad-cols*2*rad)/2
+    rows=14#int(h/(2*rad))&~1
+    border_y = 108#(h-rows*2*rad)/2
+
+    circle = [[0 for x in range(10)] for x in range(UNITS_NUM)]
+    for unit in range(HALF_UNITS_NUM):
+        ti_restart = True
+        circle[unit][0]=unit
+        circle[unit+HALF_UNITS_NUM][0]=unit+HALF_UNITS_NUM
+        while ti_restart:
+            ti_restart=False
+            num = random.randint(0,4294967295)%h
+            circle[unit][3]= num-(num%(rad*2))+rad
+            circle[unit+HALF_UNITS_NUM][3]=h-1-circle[unit][3]
+            j1 = int(circle[unit][3]/(rad*2))
+            j2 = int(circle[unit+HALF_UNITS_NUM][3]/(rad*2))
+            circle[unit][3]+=border_y
+            
+            if not unit:
+                circle[unit][2]=rad
+                if j1&1:
+                    ti_restart=True
+            else:
+                num = random.randint(0,4294967295)%(w-rad*2*2)/2
+                circle[unit][2]= num-num%(rad*2)#math.floor((random.randrange(0,4294967295)%((w-rad*2*2)/2))-(rad*2))+rad
+        circle[unit+HALF_UNITS_NUM][2]=w-1-int(circle[unit][2])
+
+        if j1&1:
+            circle[unit][2]+=rad
+        if j2&1:
+            circle[unit+HALF_UNITS_NUM][2]+=rad
+        circle[unit][2]+=border_x
+        circle[unit+HALF_UNITS_NUM][2]-=border_x
+
+        def S2Circle(x,y):
+            
+            tempY=int((y-border_y)/(rad*2))
+            if tempY&1:
+                tempX=(x-rad-border_x)/(rad*2)
+            else:
+                tempX=(x-border_x)/(rad*2)
+            return tempX,tempY
+
+        def Circle2S(x,y):
+            
+            print(x,y)
+            tempY=y*rad*2+rad+border_y
+            tempX=x*rad*2+rad+border_x
+            if int(tempY)&1:
+                tempX+=rad
+            return tempX,tempY
+
+        circle[unit][2],circle[unit][3]=S2Circle(circle[unit][2],circle[unit][3])
+        circle[unit][2],circle[unit][3]=Circle2S(circle[unit][2],circle[unit][3])
+
+        circle[unit+HALF_UNITS_NUM][2],circle[unit+HALF_UNITS_NUM][3]=S2Circle(circle[unit+HALF_UNITS_NUM][2],circle[unit+HALF_UNITS_NUM][3])
+        circle[unit+HALF_UNITS_NUM][2],circle[unit+HALF_UNITS_NUM][3]=Circle2S(circle[unit+HALF_UNITS_NUM][2],circle[unit+HALF_UNITS_NUM][3])
+
+        circle[unit][4]=0
+        circle[unit+HALF_UNITS_NUM][4]=1
+
+        circle[unit][7]=True
+        circle[unit+HALF_UNITS_NUM][7]=True
+
+        if not unit:
+            circle[unit][6]=LIGHT_CYAN
+            circle[unit+HALF_UNITS_NUM][6]=LIGHT_MAGENTA
+            circle[unit][9]=True
+            circle[unit+HALF_UNITS_NUM][9]=True
+        else:
+            circle[unit][6]=DARK_CYAN
+            circle[unit+HALF_UNITS_NUM][6]=DARK_MAGENTA
+            circle[unit][9]=False
+            circle[unit+HALF_UNITS_NUM][9]=False
+
+    for unit in range(HALF_UNITS_NUM):
+        if circle[unit][9]==True:
+            circle[unit][5]=unit
+            circle[unit+HALF_UNITS_NUM][5]=unit+HALF_UNITS_NUM
+        else:
+            best_dd=sys.maxsize
+            for piece in range(HALF_UNITS_NUM):
+                if circle[unit][0]!=circle[piece][0]:
+                    dd=pow(circle[unit][2]-circle[piece][2], 2)+pow(circle[unit][3]-circle[piece][3], 2)
+                    if circle[piece][2]<circle[unit][2] or circle[piece][9] and dd<best_dd:
+                        best_dd=dd
+                        best=piece
+            circle[unit][5]=best
+            circle[unit+HALF_UNITS_NUM][5]=best+HALF_UNITS_NUM
+    num_alive=[HALF_UNITS_NUM]*2
+    new_circle = create_circle(circle[0])
+    print(new_circle)
+    result = [new_circle]
+
+    for unit in range(1,UNITS_NUM):
+        new_circle = create_circle(circle[unit])
+        print(new_circle)
+        result.append(new_circle)
+    
+    return result
+
+def create_circle(circle:list) -> Unit:
+    return Unit(circle[0],(circle[2],circle[3]),circle[2],circle[3],circle[4],circle[5],circle[6],circle[7], 30.0,circle[9])
+
+        
