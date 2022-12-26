212a88a817d1dc460e142095ecc5fdd4197657c8
Sancheza38 <93687081+Sancheza38@users.noreply.github.com>
2022-12-25 22:02:40 -0600

    added game over event, and varying game sizes
    added the criteria for a game over and added the selection of the game size that determines all the constants for the game depending on the game size that was picked at the beginning.

--- wip/constants.py
+++ wip/constants.py
@@ -1,7 +1,20 @@
 from collections import namedtuple
 import pygame as pg
+import math
 import os
+
+repeat=True
+while repeat:
+    repeat=False
+    val = int(input("Enter game size between 1 and 9\n"))
+
+    if val<1 or val>9:
+        print("invalid input")
+        repeat=True
+
 pg.init()
+
+# font
 _FONT_PATH = os.path.join("wip","ModernDOS8x8.ttf")
 FONT = pg.font.Font(_FONT_PATH, 31)
 
@@ -20,5 +33,14 @@ PLAYER_ONE = Player(LIGHT_CYAN, 0, FONT.render("Player 1", False, DARK_CYAN))
 PLAYER_TWO = Player(LIGHT_MAGENTA, 1, FONT.render("Player 2", False, DARK_MAGENTA))
 
 # game constants
-UNITS_NUM = 18
-RAD = 30
+COLUMN_ROW = [(18,16),(20,16),(20,16),(22,18),(26,20),(28,22),(32,24),(36,28),(42,32)]
+TOTAL_UNITS = [18, 20, 24, 28, 32, 40, 50, 64, 82]
+GAME_OVER = FONT.render("Game Over", False,BLACK)
+WIDTH = 1280
+HEIGHT = 960
+UNITS_NUM = TOTAL_UNITS[val-1]
+RAD = (16-val)*2
+CIRCUMCIRCLE_RAD = RAD*2/math.sqrt(3)
+TILE_SIZE = COLUMN_ROW[val-1]
+GAME_BORDER= (int((WIDTH-(RAD*2*TILE_SIZE[0])+RAD)/2),int((HEIGHT-(CIRCUMCIRCLE_RAD*3*(TILE_SIZE[1]/2))-RAD/2)/2))
+NUM_ALIVE = [UNITS_NUM/2,UNITS_NUM/2]
